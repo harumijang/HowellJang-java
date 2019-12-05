d17e55f97770eb3ba89048f42c416ef6f5cc69a9
harumijang <h.jang521@gmail.com>
2019-12-05 04:51:17 -0500

    cross origin

--- src/main/java/com/example/videogame_java/controllers/ReviewController.java
+++ src/main/java/com/example/videogame_java/controllers/ReviewController.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 ;import java.util.List;
 
-@RestController
 @CrossOrigin(origins = "*")
+@RestController
 
 public class ReviewController {
    @Autowired
--- src/main/java/com/example/videogame_java/controllers/UserController.java
+++ src/main/java/com/example/videogame_java/controllers/UserController.java
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
   ;import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
-    @CrossOrigin(origins = "*")
     public class UserController {
       @Autowired
       DevRepository devRepository;
